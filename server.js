fe7218e3f17782b8dee518865119d155db7e14cc
Nikkala Thomson <tho15058@byui.edu>
2019-07-06 21:03:42 -0400

    testing

--- server.js
+++ server.js
@@ -45,3 +45,41 @@ app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
 })
 
+/******************************
+Functions (should put in separate file later)
+******************************/
+
+function get_game(req, res) {
+    var game = req.query.boardgame;
+    get_game_from_db(game, function (error, result) {
+        if (error || result == null) {
+            res.status(500).json({
+                success: false,
+                data: error
+            })
+        } else {
+            console.log("Back from the get_game_from_db with result:", result);
+            const params = result[0];
+            res.render('pages/display_game', params);
+        }
+    });
+
+}
+
+function get_game_from_db(game, callback) {
+
+    var sql = "SELECT name, image_url, properties FROM board_game WHERE board_game = $1::int";
+    var params = [game];
+
+    pool.query(sql, params, function (err, result) {
+        if (err) {
+            console.log("An error with the DB occurred");
+            console.log(err);
+            callback(err, null);
+        }
+
+        console.log("Found DB result: " + JSON.stringify(result.rows));
+
+        callback(null, result.rows);
+    })
+}

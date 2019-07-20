e1a0d1e0a2cf81582fd9fde7e9871f1b9483c157
Nikkala Thomson <tho15058@byui.edu>
2019-07-20 00:30:18 -0400

    testing

--- server.js
+++ server.js
@@ -148,13 +148,13 @@ function get_game(req, res) {
                     var game_data_values = Object.values(game_data);
                     game = game_data_values[0];
                     game_score = 0;
-                    game_min_players = game_data_values[1];
+                    game_min_players = game_data_values[2];
                     console.log(game_min_players);
-                    game_max_players = game_data_values[2];
-                    game_min_playtime = game_data_values[3];
+                    game_max_players = game_data_values[3];
                     game_min_playtime = game_data_values[4];
-                    game_min_weight = game_data_values[5];
+                    game_min_playtime = game_data_values[5];
                     game_min_weight = game_data_values[6];
+                    game_min_weight = game_data_values[7];
 
                     // adjust game score for number of players
                     if (!((game_max_players < min_players) OR(game_min_players > max_players))) {

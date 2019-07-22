7a2ba217a13784c128fdc654664a7381638b087a
Nikkala Thomson <tho15058@byui.edu>
2019-07-22 15:54:52 -0400

    testing

--- server.js
+++ server.js
@@ -358,28 +358,28 @@ function register(params, res, callback) {
     });
     // get gamer's gamer id number
 
-    var gamer_id = function (req, res) {
-        get_gamer_id(username, function (err, rows) {
-            if (err)
-                return next(err);
-            console.log(rows);
-            res.json(rows);
-            res.send(rows[0].gamer);
-        });
-    };
+    get_gamer_id(username, function (err, res) {
+        if (err) {
+            return next(err);
+        } else {
+            const gamer_id = res;
+            console.log("gamer_id" + gamer_id);
+        }
+    });
+
 
     console.log('Gamer id', gamer_id);
-    /*
-       default_prefs = '{"min_players":2, "max_players":4, "min_playtime":30, "max_playtime":120, "min_weight":1.5, "max_weight":2.5, "themes":[], "mechanisms":[]}';
-       // create default game preferences for gamer
-       var sql3 = "INSERT INTO preference(gamer, preferences) VALUES ($1, $2)";
-       pool.query(sql3, [gamer_id, default_prefs], function callback(err, result) {
-           if (err) {
-               console.log("An error with the DB occurred in default prefs.");
-               console.log(err);
-               callback(err, null);
-           }
-       }); */
+
+    default_prefs = '{"min_players":2, "max_players":4, "min_playtime":30, "max_playtime":120, "min_weight":1.5, "max_weight":2.5, "themes":[], "mechanisms":[]}';
+    // create default game preferences for gamer
+    var sql3 = "INSERT INTO preference(gamer, preferences) VALUES ($1, $2)";
+    pool.query(sql3, [gamer_id, default_prefs], function callback(err, result) {
+        if (err) {
+            console.log("An error with the DB occurred in default prefs.");
+            console.log(err);
+            callback(err, null);
+        }
+    });
 
     res.redirect('/games');
 } // end of register

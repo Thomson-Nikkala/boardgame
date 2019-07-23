3d3d403e9cf060a6435559a91fc070533e47348f
Nikkala Thomson <tho15058@byui.edu>
2019-07-23 02:38:39 -0400

    testing

--- server.js
+++ server.js
@@ -253,7 +253,8 @@ function get_game(req, res) {
                     })
                 } else {
                     console.log("Back from the get_game_from_db with result:", res3);
-                    const the_game = res3[0];
+                    const the_game = res3;
+                    console.log("the_game", the_game);
                     if (sess.username !== "Guest") {
                         sql = "INSERT INTO recommendation (username, board_game) VALUES ($1, $2);";
                         pool.query(sql, [sess.username, the_game], function callback(err, result) {
@@ -322,7 +323,7 @@ function get_game_from_db(game, callback) {
             callback(err, null);
         }
 
-        callback(null, result.rows);
+        callback(null, result.rows[0]);
     });
 }
 

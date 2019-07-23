1fb76eb1772f1b7c763f467259916a4caaf1885f
Nikkala Thomson <tho15058@byui.edu>
2019-07-23 04:29:58 -0400

    testing

--- server.js
+++ server.js
@@ -429,8 +429,8 @@ function go_login_err(req, res) {
 function login(params, res, callback) {
     // var username1 = $("#username").val();
     // var password1 = $("#password").val();
-    var username1 = params.username;
-    var password1 = params.password;
+    const username1 = params.username;
+    const password1 = params.password;
     var hashed_password = '';
     console.log('username is ', username1);
     console.log('password is ', password1);
@@ -449,36 +449,36 @@ function login(params, res, callback) {
     });
 
 
-    /*var new_gamer = result.rows[0];
-                console.log('new_gamer'+new_gamer);
-                if (new_gamer) {
-                    sess.username = username1;
-                    res.redirect('/gamer');
-                } else {
-                    res.redirect('/loginerr');
-                }
+    var new_gamer = result.rows[0];
+    console.log('new_gamer' + new_gamer);
+    if (new_gamer) {
+        sess.username = username1;
+        res.redirect('/gamer');
+    } else {
+        res.redirect('/loginerr');
+    }
 
-   /* bcrypt.hash(password1, salt_rounds, function (err, hash) {
-        var sql = "SELECT * FROM gamer WHERE username=$1 AND hashed_password=$2";
-        pool.query(sql, [username1, hash], function callback(err, result) {
-            if (err) {
-                console.log("An error with the DB occurred in login()");
-                console.log(err);
-                callback(err, null);
-            } else {
-                console.log(result);
-                new_gamer = result.rows[0];
-                console.log(new_gamer);
-                if (new_gamer) {
-                    sess.username = username1;
-                    res.redirect('/gamer');
-                } else {
-                    res.redirect('/loginerr');
-                }
-            }
-        }) 
+    /* bcrypt.hash(password1, salt_rounds, function (err, hash) {
+         var sql = "SELECT * FROM gamer WHERE username=$1 AND hashed_password=$2";
+         pool.query(sql, [username1, hash], function callback(err, result) {
+             if (err) {
+                 console.log("An error with the DB occurred in login()");
+                 console.log(err);
+                 callback(err, null);
+             } else {
+                 console.log(result);
+                 new_gamer = result.rows[0];
+                 console.log(new_gamer);
+                 if (new_gamer) {
+                     sess.username = username1;
+                     res.redirect('/gamer');
+                 } else {
+                     res.redirect('/loginerr');
+                 }
+             }
+         }) 
 
 
-    });*/
+     });*/
 
 } // end login
